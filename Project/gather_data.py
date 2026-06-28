a54d25a57cd05ba56fafae752ff00f56e25a012d
Alex Gilmore <AJGilmore00@hotmail.com>
2026-06-28 15:42:05 +0100

    created function for gathering data

--- Project/gather_data.py
+++ Project/gather_data.py
@@ -1,12 +1,70 @@
-import pandas
+import pandas as pd
+from urllib.parse import urlparse
+import pathlib
 
+from SerpAPI.prompt import Prompt
+from SerpAPI import prompter
 from SerpAPI.locations import LOCATIONS
 from Project.prompt_list import PROMPTS
 
-def gather_data() -> None:
-    for partial_id, prompt in PROMPTS.items():
+
+def gather_data(output_path: str = "Project/Data/raw_data.csv") -> None:
+    if prompter.get_remaining_searches() < len(PROMPTS) * len(LOCATIONS) * 2:
+        print("[data log][WARNING] insufficient remaining searches to gather all requested data")
+        return None
+
+    rows = []
+
+    for partial_id, query in PROMPTS.items():
         for location in LOCATIONS:
-            pass
+            prompt_id = generate_prompt_id(partial_id, location)
+            prompt = Prompt(prompt_id, query, location)
+            references = prompter.retrieve_ai_overview_references(prompt)
+
+            if not references:
+                print(f"[prompt log][FAILURE] failed to find references for prompt '{prompt_id}', " \
+                    "this prompt has been passed over")
+                continue
+
+            print("[prompt log][SUCCESS] found references, creating datapoints...")
+
+            succ_ref_count = 0
+            fail_ref_count = 0
+            for position, reference in enumerate(references, start=1):
+                link = reference.get("link", "")
+                if not link:
+                    fail_ref_count += 1
+                    continue
+                succ_ref_count += 1
+
+                rows.append({
+                    "prompt_id": prompt_id,
+                    "position": position,
+                    "domain": extract_domain(link),
+                    "title": reference.get("title"),
+                    "full_link": link,
+                    "full_query": query,
+                    "full_send_location": location
+                })
+            print(f"[data log] created {succ_ref_count} data points. skipped {fail_ref_count} " \
+                    "references with irretrievable links")
+            
+    df = pd.DataFrame(rows)
+
+    pathlib.Path(output_path).parent.mkdir(parents=True, exist_ok=True)
+    df.to_csv(output_path, index=False)
+    print(f"[data log][FINISHED] saved {len(df)} rows to '{output_path}'")
+
+
+# extracts the domain given a link. also removes 'www.' if present
+# if link is invalid, return empty string
+def extract_domain(link: str) -> str:
+    if not link:
+        return ""
+    domain = urlparse(link).netloc
+    if domain.startswith("www."):
+        domain = domain[4:]
+    return domain
 
 
 def generate_prompt_id(partial_id: str, location: str) -> str:
