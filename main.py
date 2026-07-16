886ca918f71fbd7cb9dcd5dd8905f2391af3ae46
Alex Gilmore <AJGilmore00@hotmail.com>
2026-07-16 14:27:09 +0100

    updated gitignore

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from Project import gather_data, structure_data
 
 def main():
     # run this to test your API key, should print your remaining credits
-    #tests.test_prompt_id_generation()
+    tests.test_connection()
 
     # run this ONCE to gather the required data
     # gather_data.gather_data()
 
     # run this after running gather_data() to create a new csv grouping prompts by type
     # accompanied by a tally count for every domain referenced within that group
-    structure_data.group_by_prompt()
+    # structure_data.group_by_prompt()
 
 
 if __name__ == "__main__":
