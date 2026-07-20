6cafdb7bb7fb8b1b560f00bac3d5d6dfb3dbcf82
Alex Gilmore <AJGilmore00@hotmail.com>
2026-07-20 11:47:50 +0100

    added semantic grouping lists for domains

---
+++ Project/domain_groups.py
@@ -0,0 +1,9 @@
+# semantic groups of domains based off of their main content themes and ideas
+
+SOCIALMEDIA_FORUM = ["reddit.com", "facebook.com" "instagram.com", "x.com", "youtube.com", 
+                     "quora.com"]
+
+TOP_UK_THIRDPARTY_BOOKERS = ["haystravel.co.uk", "hoseasons.co.uk", "sykescottages.co.uk",
+                             "booking.com", "airbnb.co.uk", "cottages.com", "cottages-and-castles.co.uk",
+                             "expedia.co.uk", "holidaycottages.co.uk", "lastminute.com", "onthebeach.co.uk",
+                             "tripadvisor.co.uk"]
